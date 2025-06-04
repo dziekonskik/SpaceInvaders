98851429a504d516af14a137c36c79445d3388b7
Konrad Dziekonski <jaseveen@gmail.com>
2025-06-04 07:30:24 +0200

    add size and position class

---
+++ src/utils/Size.java
@@ -0,0 +1,18 @@
+package utils;
+
+public class Size {
+    private int width,height;
+
+    public Size(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
