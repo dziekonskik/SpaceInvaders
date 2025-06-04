832fc607103d45cd864d7e2ab46179e66df678a3
Konrad Dziekonski <jaseveen@gmail.com>
2025-06-04 21:14:07 +0200

    add font manager class

---
+++ src/utils/FontManager.java
@@ -0,0 +1,20 @@
+package utils;
+import java.awt.*;
+import java.io.InputStream;
+
+public class FontManager {
+    private static Font vt323;
+
+    public static Font getVT323(float size) {
+        if (vt323 == null) {
+            try {
+                InputStream is = FontManager.class.getResourceAsStream("/resources/VT323-Regular.ttf");
+                assert is != null;
+                vt323 = Font.createFont(Font.TRUETYPE_FONT, is);
+            } catch (Exception e) {
+                vt323 = new Font("Monospaced", Font.BOLD, 24);
+            }
+        }
+        return vt323.deriveFont(size);
+    }
+}
